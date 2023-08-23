f61196f42fa9d4db8de32eed286ff45b168bf363
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-08-22 21:25:01 -0500

    updated files

--- app.js
+++ app.js
@@ -2,4 +2,7 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
+// ADD THESE TWO LINES
+var UserController = require('./user/user_controller');
+app.use('/users', UserController);
 module.exports = app;
