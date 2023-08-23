621ec9db0fafce05231b9c8986eb555fbf67cf26
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-08-22 21:46:04 -0500

    updated files

--- user/user_controller.js
+++ user/user_controller.js
@@ -9,15 +9,15 @@ var User = require('./user');
 // CREATES A NEW USER
 router.post('/', function (req, res) {
   User.create({
-      name : req.body.name,
-      email : req.body.email,
-      password : req.body.password
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
     },
     function (err, user) {
       if (err) return res.status(500).send("There was a problem adding the information to the database.");
       res.status(200).send(user);
-    });
-});
+    }).then(r =>);
+ });
 // RETURNS ALL THE USERS IN THE DATABASE
 router.get('/', function (req, res) {
   User.find({}, function (err, users) {
