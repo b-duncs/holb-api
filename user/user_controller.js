7d4cb10c6c48cbd4c9375dfd5e13dedaf3becb2b
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-08-22 22:15:06 -0500

    updated files

--- user/user_controller.js
+++ user/user_controller.js
@@ -4,14 +4,14 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
-var User = require('./User');
+var User = require('./user');
 // ADD THIS PART
 // CREATES A NEW USER
-router.post('/', async function (req, res) {
-  await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
+router.post('/', function (req, res) {
+  User.create({
+      name : req.body.name,
+      email : req.body.email,
+      password : req.body.password
     },
     function (err, user) {
       if (err) return res.status(500).send("There was a problem adding the information to the database.");
